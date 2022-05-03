c0114132fdfe18dc7ae9e3b08b8f0dfdbfec92a6
Caitlin Stevenson <caitlinstevenson@Caitlins-Air.attlocal.net>
2022-05-03 17:41:33 -0500

    got app working.

--- client/src-sw.js
+++ client/src-sw.js
@@ -71,8 +71,9 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
   If you supply the correct values below, this file is complete.
 */
 
-registerRoute( ({ request }) => variableForArrayHere.ARRAY_METHOD_HERE(variableForDestinationHere),
-  new CLASSNAME_TO_INSTANTIATE_HERE({
+pathRoute( 
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({
     cacheName: 'asset-cache',
     plugins: [
       new CacheableResponsePlugin({
